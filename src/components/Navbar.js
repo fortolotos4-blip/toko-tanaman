891f526cf7d24685dc334988a2d69cd9e0ccdc62
Rohani Game Dev <fortolotos4@gmail.com>
2026-03-09 10:56:38 +0700

    salah coding

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <nav className="w-full bg-[#7FC4A6] text-white p-4 flex justify-between">
 
-      <div className="flex gap-6 bg-center">
+      <div className="flex gap-6 justify-center flex-1">
         <a href="/">Home</a>
         <a href="/produk">Produk</a>
         <a href="/tentang">Tentang</a>
