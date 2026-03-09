c575fe23de8ba3a761f4e628a4f5828cd0c3b25a
Rohani Game Dev <fortolotos4@gmail.com>
2026-03-09 10:54:04 +0700

    Ganti Posisi Navbar agar dinamis

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,8 @@
 export default function Navbar() {
   return (
     <nav className="w-full bg-[#7FC4A6] text-white p-4 flex justify-between">
-      <h1 className="font-bold text-xl">🌿 Tanaman Hias Rahayu</h1>
 
-      <div className="flex gap-6">
+      <div className="flex gap-6 bg-center">
         <a href="/">Home</a>
         <a href="/produk">Produk</a>
         <a href="/tentang">Tentang</a>
